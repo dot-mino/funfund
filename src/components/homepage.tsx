c06432e9e2d39902883af3f656f9d9f296bbb7c4
Xhufg <minardi.alessio99@gmail.com>
2024-04-13 18:02:35 +0200

    show campaigns

--- src/components/homepage.tsx
+++ src/components/homepage.tsx
@@ -4,7 +4,28 @@ import { Button, Input } from "@nextui-org/react";
 import { Textarea } from "@nextui-org/react";
 import { ethers } from "ethers";
 import { useEthersSigner } from '../components/adapters'
+import { useEthersProvider } from '../components/adapters'
 import FunFundContract from "../../artifacts/contracts/funfund.sol/funfund.json";
+enum CampaignStatus {
+  Active = 0,
+  Pending = 1,
+  Success = 2,
+  Deleted = 3
+}
+
+interface Campaign {
+  id: number;
+  creator: string;
+  title: string;
+  desc: string;
+  img: string;
+  goal: number;
+  endAt: number;
+  _status: CampaignStatus; // Puoi definire un tipo enum se lo hai definito nel contratto
+  donors: string[];
+  donorsContribution: number[];
+  amountCollected: number;
+}
 
 export default function Homepage() {
   const [title, setTitle] = useState("");
@@ -14,11 +35,15 @@ export default function Homepage() {
   const [date, setDate] = useState("");
   const [time, setTime] = useState("")
   const [loading, setLoading] = useState(false);
+  const [campaigns, setCampaigns] = useState<Campaign[]>([]);
+  const [campaignsLoaded, setCampaignsLoaded] = useState(false);
 
   const account = useAccount().address;
   const contractAddress = "0xa8Dc528037C7C13fbe5e12f5930D6f0b0016d11F";
   const signer = useEthersSigner()
-  console.log(signer)
+  const provider = useEthersProvider()
+  
+
 
 
   const handleCreateCampaign = async () => {
@@ -27,7 +52,6 @@ export default function Homepage() {
 
       const contract = new ethers.Contract(contractAddress, FunFundContract.abi, signer);
       const timestamp = convertToUnixEpoch(date, time);
-      console.log("timestprma ", timestamp)
 
       // Chiamare la funzione createCampaign del contratto
       await contract.createCampaign(title, description, imageURI, goal, timestamp);
@@ -45,10 +69,41 @@ export default function Homepage() {
     }
   };
 
+  const getAllCampaigns = async () => {
+    try {
+      const contract = new ethers.Contract(contractAddress, FunFundContract.abi, provider);
+
+      // Chiamare la funzione getAllCampaigns del contratto
+      const campaignList = await contract.getAllCampaigns();
+
+      // Aggiorna lo stato locale con la lista di campagne ottenuta dal contratto
+      setCampaigns(campaignList);
+      setCampaignsLoaded(true);
+    } catch (error) {
+      console.error("Error receiving campaign:", error);
+    }
+  };
+
   const convertToUnixEpoch = (date: any, time: any) => {
     const selectedDateTime = new Date(`${date}T${time}`);
     return selectedDateTime.getTime() / 1000; // Converti in secondi
   };
+  function unixEpochToDateTime(unixEpoch : any) {
+    // Crea un oggetto Date utilizzando il timestamp Unix Epoch
+    const date = new Date(unixEpoch * 1000); // Moltiplica per 1000 per convertire da secondi a millisecondi
+  
+    // Estrai giorno, mese, anno, ore e minuti dall'oggetto Date
+    const day = date.getDate();
+    const month = date.getMonth() + 1; // I mesi sono indicizzati da 0 a 11, quindi aggiungi 1
+    const year = date.getFullYear();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+  
+    // Formatta la data e l'ora in una stringa leggibile
+    const formattedDateTime = `${day}/${month}/${year} ${hours}:${minutes}`;
+  
+    return formattedDateTime;
+  }
   return (
     <div className="grid grid-cols-2 gap-5 text-black flex justify-center">
       <div className="flex justify-center mt-6">
@@ -120,7 +175,25 @@ export default function Homepage() {
         </div>
       </div>
       <div className="">
-        <p>test2</p>
+        <Button
+          className="mt-2 self-end"
+          color="default"
+          onClick={getAllCampaigns}
+          disabled={loading}
+        >
+          Get All Campaigns
+        </Button>
+        {/* Visualizza le campagne */}
+        {campaignsLoaded && campaigns.map((campaign, index) =>  (
+          <div key={index} className="mt-4 p-4 border border-gray-200 rounded">
+            <h2>Title: {campaign.title}</h2>
+            <p>Description: {campaign.desc}</p>
+            <p>Goal: {campaign.goal.toString()} ETH</p>
+            <p>Image : {campaign.img}</p>
+            <p>EndTime : {unixEpochToDateTime(campaign.endAt.toString())}</p>
+            {/* Altri campi della campagna */}
+          </div>
+        ))}
       </div>
     </div>
   );
